9b6fcc0e9fb7c7578d9e9dd5524ff3282423b083
Ajit Nath <nath.ajit01@gmail.com>
2019-03-29 00:06:32 +0800

    Reads .asm file into a string

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,34 @@
 /// Translates hack assembly language code to machine instructions. More information
 /// on hack computer can be found at: https://www.nand2tetris.org
 
+use std::io;
 use std::env;
 use std::fs;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     if args.len() != 2 {
-        panic("Bad command line arguments")
+        panic!("Bad arguments - expected file path");
     }
 
-    let asm_f = &args[1].clone();
-    contents = AssemblyFile::read(asm_f)
-
+    let asm_f = args[1].to_string();
+    let contents = read_assembly(asm_f).expect("Something went wrong");
     println!("The file contents: \n{}", contents);
 }
 
 
-impl AssemblyFile {
-    // - A hack assembly file is
-    // - The file always has the extension .asm
+fn read_assembly(asm_f: String) -> Result<String, io::Error> {
+    // Reads an .asm and output a Result with file content
+    // if read is sucessful, otherwise returns a string literal
+    // with the error message
+    let temp_f = asm_f.clone();
+    let parts: Vec<&str> = temp_f.split(".").collect();
 
-    fn read(asm_f: &String) -> Result<String, Error> {
-        let (&basename, &extension) = asm_f.split(".");
-
-        if extension != ".asm" {
-            Err("Unknown extension {} bad input file", extension);
-        }
-
-        let mut content = String::new();
-
-        match asm_f.read_to_string(&mut content) {
-            Ok(_) => Ok(content),
-            Err(e) => Err(e)
-        }
+    if parts.len() != 2 || parts[1] != "asm" {
+        panic!("Bad input file! {}\n", temp_f)
     }
+
+    fs::read_to_string(asm_f)
 }
