42377c206a57988093c0d3dfdf8aa5e5904d0b3e
Ajit Nath <nath.ajit01@gmail.com>
2019-03-31 23:52:41 +0800

    Clean up comments [WIP]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 /// Translates hack assembly language code to machine instructions. More information
 /// on hack computer can be found at: https://www.nand2tetris.org
 
+
 use std::io;
 use std::env;
 use std::fs;
@@ -16,7 +17,7 @@ fn main() {
 
     let asm_f = args[1].to_string();
     let contents = read_assembly(asm_f).expect("Something went wrong");
-    println!("The file contents: \n{}", contents);
+    parser::parse_content(contents);
 }
 
 
@@ -32,4 +33,71 @@ fn read_assembly(asm_f: String) -> Result<String, io::Error> {
     }
 
     fs::read_to_string(asm_f)
+}
+
+
+mod parser {
+
+    struct ParsedLine {
+        line: String,  // Line number associated with the line
+        inst: String,  // Instruction type whether A, L or C
+        symb: String,  // Symbol if it is an A instruction or Label
+        dest: String,  // Destination for a C instruction
+        comp: String,  // Computation if it's a C instruction
+        jump: String   // Jump if it is an C instructionj
+    }
+
+
+    pub fn parse_content(f_content: String) {
+        // Parses the assembly file line by line and
+        // returns a vector of ParsedLine
+        let line_iter = f_content.split("\n");
+        let mut line_iter = line_iter.map(|l| l.trim());
+        line_iter = line_iter.map(|l|
+                                  {
+                                      l.trim();
+                                      remove_comments(l);
+                                  }
+
+        );
+        let line_iter = line_iter.filter(|l| l.len() > 0);
+
+        for (linenum, line) in line_iter.enumerate() {
+            let i_type = get_instruction_type(line);
+            println!("{} - {} - {}", linenum, i_type, line);
+        }
+    }
+
+
+    // fn clean_line(line: &str) -> &'static str {
+    //     line = line.trim();
+    //     let char_vect: Vec<char> = line.chars().collect();
+    //     let mut clean_line = String::new();
+    // }
+
+    fn remove_comments(line: &str) -> String {
+        let chars  = line.chars();
+        let mut cleaned = String::new();
+
+        for (idx, ch) in chars.enumerate() {
+            cleaned.push_str(&ch.to_string());
+        }
+
+        cleaned
+    }
+
+
+    fn get_instruction_type(line: &str) -> &'static str {
+        // Checks whether it is A, L or C type instruction
+        let char_vect: Vec<char> = line.chars().collect();
+        let first_char = char_vect[0];
+
+        if first_char == '@' {
+            "A"
+        } else if first_char == '(' {
+            "L"
+        } else {
+            "C"
+        }
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,48 +1,59 @@
-/// Parser:
-/// Parses each line in the assembly code
-///
-/// For each line:
-///     - Find the command type A_TYPE (address) C_TYPE (command) or L_TYPE (label)
-///     - symbol - gets the symbol if it's an A or L instruction
-///     - dest - Destination if it's a C instruction
-///     - comp - Computation mneumonic if it' a C instruction
-///     - jmp  - Jump mneumonic if a jump mneumonic is included
-///
-/// Note:
-/// Whitespaces and comments (starting //) are ignored.
-/// Assume that the code has no syntax errors
+pub mod parser {
+    /// Parses each line in the assembly code
+    ///
+    /// For each line:
+    ///     - Find the command type A_TYPE (address) C_TYPE (command) or L_TYPE (label)
+    ///     - symbol - gets the symbol if it's an A or L instruction
+    ///     - dest - Destination if it's a C instruction
+    ///     - comp - Computation mneumonic if it' a C instruction
+    ///     - jmp  - Jump mneumonic if a jump mneumonic is included
+    ///
+    /// Note:
+    /// Whitespaces and comments (starting //) are ignored.
+    /// Assume that the code has no syntax errors
+
+    // struct ParsedAssembly {
+
+    //     instruction_type: String, // Instruction type whether A, L or C
+    //     symbols: Vec<String>,
+    //     dest: String,
+    //     comp: String,
+    //     jmp: String,
+    // }
 
 
-pub fn parse(file_content: String) {
-    // Parse file resulting in an array
-}
+    fn parse_contents()  {
+        // Parse file resulting in an array
+        println!("Parsing");
+    }
 
 
-fn parse_line(line: String) {
-    // Parse a line of assembly
-}
+    // fn parse_line(line: String) {
+    //     // Parse a line of assembly
+    // }
 
 
-fn trim_line(line: String) {
-    // Removes whitespaces and comments from the instruction
-}
+    // fn trim_line(line: String) {
+    //     // Removes whitespaces and comments from the instruction
+    // }
 
 
-fn get_instruction_type(line: String) -> char{
-    // Checks for A instruction, C instruction or L instruction
-}
+    // fn get_instruction_type(line: String) -> char{
+    //     // Checks for A instruction, C instruction or L instruction
+    // }
 
 
-fn get_destination_c(line: String) -> String {
-    // Gets destination if it is a C instruction
-}
+    // fn get_destination_c(line: String) -> String {
+    //     // Gets destination if it is a C instruction
+    // }
 
 
-fn get_computation_c(line: String) -> String {
-    // Gets computation mneumonic if it's a C instruction
-}
+    // fn get_computation_c(line: String) -> String {
+    //     // Gets computation mneumonic if it's a C instruction
+    // }
 
 
-fn get_jump_c(line: String) {
-    // Gets jump  if it's a c instruction
+    //     fn get_jump_c(line: String) {
+    //         // Gets jump  if it's a c instruction
+    //     }
 }
