99c80e31ffc624fcbfee61054dc1782fe43285da
Ajit Nath <nath.ajit01@gmail.com>
2019-04-22 04:24:57 +0800

    Setup library [WIP]

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
 // Parse hack assembly code into machine language code
 
-pub struct Assembler {
-    assembly: String,
-    machine: String
+pub struct Assembler<'a> {
+    assembly: &'a str,
+    machine: String,
+}
+
+
+impl<'a> Assembler<'a> {
+
+    pub fn translate(assembly: &'a str) -> Assembler<'a> {
+
+        parsed = parse(assembly: &'a str);
+        let mut decoded = "";
+
+        for line in parsed {
+            let d = decode_inst(line);
+            decoded.push_as_str(d);
+        }
+
+        return Assembler {
+            assembly,
+            machine: d
+        }
+    }
+
 }
